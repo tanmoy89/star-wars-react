23ca0a96d7a2e1b62642df49fd5f23b4884bb7f2
sergeantgank <eminem.95@hotmail.de>
2018-06-16 19:32:57 +0200

    First test without parsing Homeworld

--- starapi/src/App.js
+++ starapi/src/App.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
 import ContentView from './contentView';
+import "./App.css" ;
 
 class App extends Component {
   componentDidMount() {
-    this.getLuke();
+    this.getLuke(this.API_URL);
   }
-
-  API_URL = "https://swapi.co/api/people/1/";
-
   state = { luke: "" };
-
-  async function getLuke() {
-    const resp = await fetch(API_URL);
+  
+  API_URL = "https://swapi.co/api/people/1/";
+  
+  getLuke = async function (url) {
+    
+    const resp = await fetch(url);
     const data =  await resp.json();
     this.setState({luke: data});
   }
     
   render() {
-    return <ContentView data={this.state.luke} />
+    return (
+    <div>
+      <h1>Star Wars</h1>
+      <ContentView data={this.state.luke} />
+    </div>
+      )
   }
 }
 
--- starapi/src/contentView.js
+++ starapi/src/contentView.js
@@ -1,15 +1,32 @@
 import React from 'react';
+import "./ContentView.css";
+
+
 
 const ContentView = ({data}) => {
-  return (
-  <div className="ba bw1 br4 shadow-3 bg-red ">
-    <p>Name: {data.name}</p>
-    <p>Born: {data.birth_year}</p>
-    <p>Gender: {data.gender}</p>
-    <p>Homeworld: {data.homeworld}</p>
-  </div>
-  );
+  // constructor(props) {
+  //   super(props);
 
+  //   this.state = {
+  //     homeworld: ""
+  //   };
+  // }
+  // componentDidMount() {
+  //   this.getHomeWorld(this.data.homeworld);
+  // }
+  // getHomeWorld = async function (url) {
+  //   const response = await fetch(url);
+  //   const data = await response.json();
+  //   this.setState({ homeworld: data });
+  // }
+  // render() {}
+    return (
+      <div className="ba bw1 br4 shadow-3 bg-red ">
+        <p>Name: {data.name}</p>
+        <p>Born: {data.birth_year}</p>
+        <p>Gender: {data.gender}</p>
+        <p>Homeworld: {data.homeworld}</p>
+      </div>)
 
-}
+    }
 export default ContentView;
